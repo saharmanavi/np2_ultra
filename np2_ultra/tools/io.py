2767255a3f7be3479e464bfdc435d69c7a9178f0
saharmanavi <saharm@alleninstitute.org>
2021-09-24 14:31:20 -0700

    fixed psth bin size issue

--- np2_ultra/tools/io.py
+++ np2_ultra/tools/io.py
@@ -38,6 +38,25 @@ def read_pxi_dict(path_to_json='default'):
 
     return pxi_dict
 
+def read_opto_params(path_to_json='default'):
+    '''
+    path_to_json: can enter an absolute path to json file in case of different opto params.
+                Custom json MUST include all the same keys/format as default.
+                  To use default path, leave as None.
+    '''
+    if path_to_json == 'default':
+        current_dir = os.getcwd()
+        os.chdir(os.path.dirname(files.__file__))
+        with open("opto_params.json", "r") as c:
+            opto_params = json.load(c)
+        os.chdir(current_dir)
+
+    elif path_to_json != 'default':
+        with open(path_to_json, "r") as p:
+            opto_params = json.load(p)
+
+    return opto_params
+
 def get_paths_to_kilosort_templates():
     '''
     Returns the locations of the kilosort template .m files for the 1.0 and ultra probe sorting.
